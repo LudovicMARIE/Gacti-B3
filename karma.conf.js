ec02a2ae7b5633f2446f63614028fbd4ed6fbd4e
Ludovic MARIE <ludovic.marie@aol.com>
2024-06-05 22:55:51 +0200

    fix ?

---
+++ karma.conf.js
@@ -0,0 +1,39 @@
+module.exports = function (config) {
+    config.set({
+      basePath: '',
+  
+      frameworks: ['mocha'],
+  
+      files: [
+        '*.js'
+      ],
+  
+      exclude: [],
+  
+      reporters: ['progress'],
+  
+      port: 9876,
+  
+      colors: true,
+  
+      logLevel: config.LOG_INFO,
+  
+      autoWatch: false,
+  
+      browsers: ['ChromeHeadlessNoSandbox'],
+        customLaunchers: {
+          ChromeHeadlessNoSandbox: {
+            base: 'ChromeHeadless',
+            flags: ['--no-sandbox']
+          }
+        },
+  
+      singleRun: false,
+  
+      plugins: [
+        require('../../'),
+        'karma-mocha'
+      ]
+    })
+  }
+  
